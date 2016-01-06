795e3b6bde82f082a03bcbe6f5e328f220ae927a
JDBumgardner <jake.bumardner@gmail.com>
2016-01-06 16:29:29 -0500

    Iunnoh, it's still not done, but stuff is happening.

--- web/app/static/js/home.js
+++ web/app/static/js/home.js
@@ -1,3 +1,13 @@
 var $ = jQuery =  require('jquery');
 
-$(document).ready(function () {});
+$(document).ready(function () {
+
+$(".structureselector").click(function(e){
+    e.preventDefault();
+    $(".ssi").hide();
+    $("#" + $(this).attr('data-id')).show();
+
+});
+
+});
+
